bc62eeceb94b9e985a63ecbd81d77df7b3d67fe7
James Clancey <james.clancey@gmail.com>
2026-02-13 15:52:13 -0900

    Add precompiled swift-syntax (release/6.1)
    Add swift-precompiled/swift-syntax as a package dependency on branch release/6.1 in Package.swift to use the precompiled SwiftSyntax artifacts. Package resolution state was updated accordingly.

--- Package.swift
+++ Package.swift
@@ -68,6 +68,8 @@ let package = Package(
         .package(url: "https://github.com/jpsim/Yams", from: "5.1.3"),
         .package(url: "https://github.com/saagarjha/unxip", from: "3.2.0"),
 
+        .package(url: "https://github.com/swift-precompiled/swift-syntax.git", branch: "release/6.1"),
+
         // TODO: just depend on tuist/XcodeProj instead
         .package(url: "https://github.com/yonaskolb/XcodeGen", from: "2.43.0"),
     ],
